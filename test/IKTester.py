dcaec69de08c8feacc711b6254a3c2a1438820ef
Praneeth.Nagareddy <praneeth@zeromoblt.com>
2026-02-27 21:49:22 +0530

    Adding servo test code

--- test/IKTester.py
+++ test/IKTester.py
@@ -1,3 +1,15 @@
 from inverse_kinematics import IK
+from adafruit_servokit import ServoKit
 
-print(IK.solve(90,10,-40))
+import time
+import sys
+
+angles = IK.solve(90,10,-40)
+
+kit = ServoKit(channels = 16, address=0x40)
+
+kit.servo[14].angle = angles[0]
+time.sleep(0.5)
+kit.servo[12].angle = angles[1]
+time.sleep(0.5)
+kit.servo[0].angle = angles[2]
