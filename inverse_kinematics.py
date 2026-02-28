94b58777055a5334c2e714dae51be0608899d68b
Praneeth.Nagareddy <praneeth@zeromoblt.com>
2026-02-28 22:14:37 +0530

    Adding coxa offset

--- inverse_kinematics.py
+++ inverse_kinematics.py
@@ -1,6 +1,7 @@
 import math
 from typing import Tuple
-from config import FEMUR_LENGTH_MM, TIBIA_LENGTH_MM, COXA_LENGTH_MM
+from config import FEMUR_LENGTH_MM, TIBIA_LENGTH_MM, COXA_LENGTH_MM, COXA_Z_OFFSET_MM
+
 
 class IK:
     @staticmethod
@@ -8,35 +9,31 @@ class IK:
         if not right_handed:
             x, y = y, x
 
-        # 1. Hip Angle (remains the same)
+        # 1. Hip Angle
         hip_angle_rad = math.atan2(y, x)
 
-        # 2. Total distance from the hip axis to the foot on the XY plane
+        # 2. Geometry Correction for Scenario Z
         h_total = math.hypot(x, y)
-
-        # 3. Effective horizontal reach for the Femur/Tibia triangle
-        # We subtract the Coxa because it's a fixed horizontal distance
         h_eff = h_total - COXA_LENGTH_MM
+        z_eff = z - COXA_Z_OFFSET_MM  # This accounts for the 'step' in the coxa
 
-        # 4. Actual reach length for the triangle (from femur start to foot)
-        l = math.hypot(h_eff, z)
+        # 3. New 'L' based on corrected coordinates
+        l = math.hypot(h_eff, z_eff)
 
         # Reachability Check
         max_reach = FEMUR_LENGTH_MM + TIBIA_LENGTH_MM
-        if l > max_reach:
-            l = max_reach
+        l = min(l, max_reach)
 
-        # 5. Solve Ankle (Law of Cosines)
+        # 4. Law of Cosines (Ankle/Tibia)
         cos_ankle = (FEMUR_LENGTH_MM ** 2 + TIBIA_LENGTH_MM ** 2 - l ** 2) / (2 * FEMUR_LENGTH_MM * TIBIA_LENGTH_MM)
         ankle_angle_rad = math.acos(max(-1, min(1, cos_ankle)))
 
-        # 6. Solve Knee
-        # Part 1: Internal angle of the femur/tibia triangle
+        # 5. Knee/Femur Angle
         knee_a = (l ** 2 + FEMUR_LENGTH_MM ** 2 - TIBIA_LENGTH_MM ** 2) / (2 * l * FEMUR_LENGTH_MM)
         knee_angle_part1 = math.acos(max(-1, min(1, knee_a)))
 
-        # Part 2: Angle of the leg elevation relative to the floor
-        knee_angle_part2 = math.atan2(z, h_eff)
+        # Use the corrected z_eff and h_eff here!
+        knee_angle_part2 = math.atan2(z_eff, h_eff)
 
         knee_angle_rad = knee_angle_part1 + knee_angle_part2
 
