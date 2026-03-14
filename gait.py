7feb9755371e1d70f0ccce8a2486d1bdc8c8851e
Praneeth.Nagareddy <praneeth@zeromoblt.com>
2026-03-14 22:57:41 +0530

    Testing frint right leg gait

--- gait.py
+++ gait.py
@@ -47,7 +47,7 @@ class TripodGait(WalkingGait):
                 leg_id = leg.config.position.value
                 is_group_a = leg_id in TRIPOD_GATE_A_GROUP  # FR, BR, ML
 
-                if not is_group_a:
+                if leg.config.position != Legs.FRONT_RIGHT: #not is_group_a:
                     continue
 
                 # Offset the timing of Group B by half a cycle
