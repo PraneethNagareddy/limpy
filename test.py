afccf19cdcc781f94b61340831ca79109d72ab94
Praneeth.Nagareddy <praneeth@zeromoblt.com>
2026-03-19 23:17:25 +0530

    Updating test script

--- test.py
+++ test.py
@@ -9,4 +9,10 @@ X = int(sys.argv[1])
 Y = int(sys.argv[2])
 Z = int(sys.argv[3])
 
-front_right_leg.move_to_position(X, Y, Z)
+front_right_leg.hip_joint.turn(X, await_completion=True)
+front_right_leg.knee_joint.turn(Y, await_completion=True)
+front_right_leg.ankle_joint.turn(Z, await_completion=True)
+
+
+
+#front_right_leg.move_to_position(X, Y, Z)
